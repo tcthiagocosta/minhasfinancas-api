b0060539a1996d617de5d4abdc31b35e97b4f056
thiago <tc.thiagocosta@gmail.com>
2020-07-22 22:17:24 -0300

    Correções até a aula 150

--- src/main/java/com/minhasfinancas/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/minhasfinancas/minhasfinancas/api/resource/LancamentoResource.java
@@ -146,4 +146,23 @@ public class LancamentoResource {
 		
 		return lancamento;
 	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obeterPorId(id).map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
 }
